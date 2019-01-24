82b12140af51f5ceb565f6c4ce1c607cf2deb383
Chris Underwood <cunderwoodmn@gmail.com>
2019-01-24 14:01:46 -0600

    edit problem table and begin input seed file

--- script/problemData.js
+++ script/problemData.js
@@ -1,9 +1,113 @@
 const problemData = [
   {
-    description: 'Create a function that add 2 numbers.',
+    description:
+      'Create a function that adds two numbers together and returns the sum of those numbers.',
+    name: 'Adding Numbers',
     arguments: ['num1', 'num2'],
-    input: [['14', '24'], ['7', '13']],
-    output: ['38', '20'],
+    input: [['2', '2'], ['4', '3']],
+    outputType: 'num',
+    output: ['4', '7'],
+    url:
+      'https://writingjavascript.org/posts/introduction-to-functions-numbers-in-javascript',
+    stageId: 1
+  },
+  {
+    description:
+      'Create a function that accepts a string argument (a name, ex: "Oliver"), and returns the personalized greeting "Hi Oliver." Of a name is present.',
+    name: 'Say Hi to Named Input',
+    arguments: ['str'],
+    input: [['Yaodi'], ['Jay'], ['Chris'], ['MJ']],
+    outputType: 'str',
+    output: ['Hi Yaodi', 'Hi Jay', 'Hi Chris', 'Hi MJ'],
+    url:
+      'https://writingjavascript.org/posts/introduction-to-functions-numbers-in-javascript',
+    stageId: 1
+  },
+  {
+    description:
+      'Create a function that evaluates three numbers and returns the largest number out of the three. The function takes three number arguments and returns the highest number out of three.',
+    name: 'Return Largest Number',
+    arguments: ['num1', 'num2', 'num3'],
+    input: [['8', '2', '1'], ['4', '78', '2']],
+    outputType: 'num',
+    output: ['8', '78'],
+    url:
+      'https://writingjavascript.org/posts/introduction-to-functions-numbers-in-javascript',
+    stageId: 1
+  },
+  {
+    description: '',
+    name: '',
+    arguments: [],
+    input: [],
+    outputType: 'str',
+    output: [],
+    url:
+      'https://writingjavascript.org/posts/introduction-to-functions-numbers-in-javascript',
+    stageId: 1
+  },
+  {
+    description: '',
+    name: '',
+    arguments: [],
+    input: [],
+    outputType: 'str',
+    output: [],
+    url:
+      'https://writingjavascript.org/posts/introduction-to-functions-numbers-in-javascript',
+    stageId: 1
+  },
+  {
+    description: '',
+    name: '',
+    arguments: [],
+    input: [],
+    outputType: 'str',
+    output: [],
+    url:
+      'https://writingjavascript.org/posts/introduction-to-functions-numbers-in-javascript',
+    stageId: 1
+  },
+  {
+    description: '',
+    name: '',
+    arguments: [],
+    input: [],
+    outputType: 'str',
+    output: [],
+    url:
+      'https://writingjavascript.org/posts/introduction-to-functions-numbers-in-javascript',
+    stageId: 1
+  },
+  {
+    description: '',
+    name: '',
+    arguments: [],
+    input: [],
+    outputType: 'str',
+    output: [],
+    url:
+      'https://writingjavascript.org/posts/introduction-to-functions-numbers-in-javascript',
+    stageId: 1
+  },
+  {
+    description: '',
+    name: '',
+    arguments: [],
+    input: [],
+    outputType: 'str',
+    output: [],
+    url:
+      'https://writingjavascript.org/posts/introduction-to-functions-numbers-in-javascript',
+    stageId: 1
+  },
+  {
+    description: '',
+    name: '',
+    arguments: [],
+    input: [],
+    outputType: 'str',
+    output: [],
     url:
       'https://writingjavascript.org/posts/introduction-to-functions-numbers-in-javascript',
     stageId: 1
--- server/db/models/problem.js
+++ server/db/models/problem.js
@@ -9,6 +9,13 @@ const Problem = db.define('problem', {
       notEmpty: true
     }
   },
+  name: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
   arguments: {
     type: Sequelize.ARRAY(Sequelize.TEXT),
     allowNull: false,
@@ -22,6 +29,10 @@ const Problem = db.define('problem', {
     validate: {
       notEmpty: true
     }
+  },
+  outputType: {
+    type: Sequelize.ENUM,
+    values: ['num', 'str', 'bool']
   },
   output: {
     type: Sequelize.ARRAY(Sequelize.TEXT),
