9faf5deb6e6df8ba3693604b31fbea5a6edd32de
Yaodi Hu <yaodijr@gmail.com>
2019-01-24 16:16:35 -0600

    add all!@#!@$!@$

---
+++ utils/loadFunction.js
@@ -0,0 +1,22 @@
+// This utility function takes in function name and arguments, returns the function in string format for the react-ace code editor! -yh
+
+const loadFunction = (name, args) => {
+  return `function ${name}(${args.join(', ')}){
+            
+            // Your code below! (please don't edit this comment) //**//
+            
+            
+            
+            
+
+
+            
+
+
+
+            
+            
+            
+        }`
+}
+export default loadFunction
