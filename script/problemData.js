d73b2bf4c0aa1567777fdef4c1fb2f9f22d5f3fa
Jay Jongik Thom <ljik95@hotmail.com>
2019-01-24 14:20:18 -0600

    parsing data

--- script/problemData.js
+++ script/problemData.js
@@ -1,13 +1,19 @@
 const problemData = [
   {
-    question: 'Sample question 1.',
-    answer: 'blerg',
-    url: 'https://www.w3schools.com/js/js_if_else.asp'
+    description: 'Add two numbers',
+    arguments: ['num1', 'num2'],
+    funcName: 'add',
+    inputs: [['1', '2'], ['3', '5'], ['10', '20']],
+    outputs: ['3', '8', '30'],
+    dataType: ['number', 'number']
   },
   {
-    question: 'Sample question 2',
-    answer: 'meh',
-    url: 'https://www.w3schools.com/js/js_loop_for.asp'
+    description: 'Concat two arrays',
+    arguments: ['arr1', 'arr2'],
+    funcName: 'concat',
+    inputs: [['1', '2'], ['hello', 'world'], ['1, 2', '3, 4']],
+    outputs: ['1, 2', 'hello, world', '1, 2, 3, 4'],
+    dataType: ['array', 'array']
   }
 ]
 
--- server/db/models/problem.js
+++ server/db/models/problem.js
@@ -2,26 +2,51 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Problem = db.define('problem', {
-  question: {
+  description: {
     type: Sequelize.TEXT,
-    allowNull: false,
-    validate: {
-      notEmpty: true
-    }
+    allowNull: false
   },
-  answer: {
-    type: Sequelize.TEXT,
-    allowNull: false,
-    validate: {
-      notEmpty: true
-    }
+  arguments: {
+    type: Sequelize.ARRAY(Sequelize.STRING),
+    allowNull: false
   },
-  url: {
+  funcName: {
     type: Sequelize.STRING,
+    allowNull: false
+  },
+  inputs: {
+    type: Sequelize.ARRAY(Sequelize.ARRAY(Sequelize.STRING)),
     allowNull: false,
-    validate: {
-      notEmpty: true
-    }
+    get: function()  {
+      let types = this.getDataValue('arguments');
+      let inputs = this.getDataValue('inputs');
+      for (let j = 0; j < inputs.length; j++) {
+        for (let i = 0; i < types.length; i++) {
+          switch (types[i].slice(0,3)) {
+            case 'num':
+              inputs[j][i] = parseInt(inputs[j][i])
+              break;
+            case 'arr':
+              if (inputs[j][i].includes(',')) {
+                inputs[j][i] = inputs[j][i].split(', ')
+              } else {
+                inputs[j][i] = [inputs[j][i]]
+                break;
+              }
+            // case 'obj':
+            //   return inputs[j].map((input) => {
+            //   })
+            default:
+              continue
+          }
+        }
+      }
+      return inputs
+    },
+  },
+  outputs: {
+    type: Sequelize.ARRAY(Sequelize.STRING),
+    allowNull: false
   }
 })
 
