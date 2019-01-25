68ca7615bdbb7956dde33ef5c2e7ee9dd0b96928
Yaodi Hu <yaodijr@gmail.com>
2019-01-24 18:23:22 -0600

    forgot to add all

---
+++ client/store/problem.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+
+// INITIAL STATE
+const initialState = {
+  currentProblem: {yazidi: 'yahoodi'}
+}
+
+// ACTION TYPES
+const GOT_PROBLEM = 'GOT_PROBLEM'
+
+// ACTION CREATORS
+const gotProblem = data => ({
+  type: 'GOT_PROBLEM',
+  data
+})
+
+// THUNK CREATOR
+
+export const fetchProblem = id => {
+  return async function(dispatch) {
+    let {data} = await axios.get(`/api/problems/${id}`)
+    console.log('thunk>>>', data)
+    dispatch(gotProblem(data))
+  }
+}
+
+// REDUCER
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case GOT_PROBLEM:
+      return {...state, currentProblem: action.data}
+    default:
+      return state
+  }
+}
