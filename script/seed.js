962e1eb54ad6edb926467685d6bdc3970f582eb1
Jay Jongik Thom <ljik95@hotmail.com>
2019-01-25 14:22:08 -0600

    minor updates on seed data and models

--- script/seed.js
+++ script/seed.js
@@ -10,8 +10,8 @@ async function seed() {
   console.log('db synced!')
 
   const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
+    User.create({email: 'user@user.com', password: 'user'}),
+    User.create({email: 'admin@admin.com', password: 'admin', admin: true})
   ])
 
   const stages = await Promise.all([
