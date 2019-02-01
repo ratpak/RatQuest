6b84702a2c60c090045ef0fc4997dcb40ec23159
MJ Zefeldt <mjzefeldt@gmail.com>
2019-01-31 18:28:39 -0600

    fixed nav for admin

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -4,33 +4,35 @@ import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
-const Navbar = ({handleClick, isLoggedIn, email, is}) => (
-  <div className="navbar">
-    {/* <nav> */}
-    <div className="simple-flex">
-      <div id="avatar" />
-      <div id="nav-info">
-        <h1>Rat Quest</h1>
-        <h4>Welcome, {email}</h4>
+const Navbar = props => {
+  const {handleClick, isLoggedIn, email, isAdmin} = props
+  console.log(props, '<<<navbar')
+  return (
+    <div className="navbar">
+      <div className="simple-flex">
+        <div id="avatar" />
+        <div id="nav-info">
+          <h1>Rat Quest</h1>
+          <h4>Welcome, {email}</h4>
+        </div>
       </div>
-    </div>
 
-    {is && (
-      <div>
-        <Link to="/" />
-      </div>
-    )}
-    {isLoggedIn && (
-      <div>
-        {/* The navbar will show these links after you log in */}
-        <a href="#" onClick={handleClick}>
-          Logout
-        </a>
-      </div>
-    )}
-    {/* </nav> */}
-  </div>
-)
+      {isAdmin && (
+        <div>
+          <Link to="/admin">Admin</Link>
+        </div>
+      )}
+      {isLoggedIn && (
+        <div>
+          {/* The navbar will show these links after you log in */}
+          <a href="#" onClick={handleClick}>
+            Logout
+          </a>
+        </div>
+      )}
+    </div>
+  )
+}
 
 /**
  * CONTAINER
