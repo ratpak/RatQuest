740aef9ede5529e5cd7e0c878ec50f7cf373bc26
Yaodi Hu <yaodijr@gmail.com>
2019-02-01 17:08:16 -0600

    establish socket

--- client/components/multiplayer.js
+++ client/components/multiplayer.js
@@ -1,5 +1,16 @@
 import React, {Fragment} from 'react'
+import socket from '../socket'
 
-export const Multiplayer = props => {
-  return <Fragment>idk</Fragment>
+socket.on('connect', function() {
+  console.log('HAHA')
+})
+console.log('hello from mul')
+
+const Multiplayer = props => {
+  return (
+    <Fragment>
+      <button onClick={() => socket.emit('SEND', 'string')}>socket</button>
+    </Fragment>
+  )
 }
+export default Multiplayer
--- server/socket/index.js
+++ server/socket/index.js
@@ -1,7 +1,10 @@
 module.exports = io => {
   io.on('connection', socket => {
     console.log(`A socket connection to the server has been made: ${socket.id}`)
-
+    socket.on('SEND', variable => {
+      console.log('received', variable)
+      socket.broadcast.emit('send all', 'string')
+    })
     socket.on('disconnect', () => {
       console.log(`Connection ${socket.id} has left the building`)
     })
