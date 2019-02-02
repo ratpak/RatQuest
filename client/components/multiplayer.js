a0c02b00310bb36460cd0fc1c27467d1de8e9faa
Yaodi Hu <yaodijr@gmail.com>
2019-02-01 19:30:41 -0600

    stash applied

--- client/components/multiplayer.js
+++ client/components/multiplayer.js
@@ -1,18 +1,44 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, Component} from 'react'
 import socket from '../socket'
+import Axios from 'axios'
 
-socket.on('connect', function() {
+socket.on('connect', async function() {
   console.log('HAHA')
+  // console.log('socket yazidi', socket.yazidi)
+  // const {data} = await Axios.get(`/api/users/1`)
+  // console.log('data from socket on', data)
 })
 console.log('hello from mul')
 
-socket.on('send all', string => console.log('received send all', string))
+class Multiplayer extends Component {
+  constructor() {
+    super()
+    this.state = {me: 0, opponent: 0}
+    this.handleClick = this.handleClick.bind(this)
+    socket.on('send all', string => {
+      console.log('how many')
+      this.setState({opponent: this.state.opponent + 1})
+    })
+    socket.on('notice', message => {
+      console.log('notice received', message)
+    })
+    socket.on('login', user => console.log('got user>>>', user))
+  }
+  handleClick() {
+    socket.emit('SEND', 'string')
+    this.setState({me: this.state.me + 1})
+  }
 
-const Multiplayer = props => {
-  return (
-    <Fragment>
-      <button onClick={() => socket.emit('SEND', 'string')}>socket</button>
-    </Fragment>
-  )
+  render() {
+    if (this.state.me >= 10 || this.state.opponent >= 10)
+      return <h1>DONE!@#</h1>
+    return (
+      <Fragment>
+        <button onClick={this.handleClick}>socket</button>
+        <h1>Opponent:{this.state.opponent}</h1>
+        <h1>Me: {this.state.me}</h1>
+      </Fragment>
+    )
+  }
 }
 export default Multiplayer
--- server/socket/index.js
+++ server/socket/index.js
@@ -1,6 +1,7 @@
 module.exports = io => {
   io.on('connection', socket => {
     console.log(`A socket connection to the server has been made: ${socket.id}`)
+    socket.broadcast.emit('notice', 'message@')
     socket.on('SEND', variable => {
       console.log('received', variable)
       socket.broadcast.emit('send all', 'string')
