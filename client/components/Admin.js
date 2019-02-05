d797585c632096387d531c639d230793ed0cfedc
Chris Underwood <cunderwoodmn@gmail.com>
2019-02-05 17:57:31 -0600

    styled admin page

--- client/components/Admin.js
+++ client/components/Admin.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {deleteUser, fetchUsers, toggleisAdmin, fetchAllProblems} from '../store'
+import Button from '@material-ui/core/Button'
+import Card from '@material-ui/core/Card'
 
 class Admin extends React.Component {
   componentDidMount() {
@@ -14,6 +16,7 @@ class Admin extends React.Component {
   handleToggleisAdmin = userId => {
     this.props.toggleisAdmin(userId)
   }
+
   render() {
     const users = this.props.users.users
     const problems = this.props.problems
@@ -22,76 +25,85 @@ class Admin extends React.Component {
     const imgStyle = {
       maxWidth: '100px',
       maxHeight: '100px',
-      borderRadius: '10px',
-      marginRight: '20px'
+      borderRadius: '10px'
     }
+
+    const buttonStyle = {
+      backgroundColor: 'lightGrey',
+      margin: '3px'
+    }
+
     return (
       <div>
-        <h1>Admin page</h1>
+        <h1 id="adminCenter">Admin page</h1>
         <h3>Edit/Delete Users</h3>
-        <div>
-          <span>Avatar</span>
-          <span>E-Mail</span>
-          <span>Delete</span>
-          <span>Admin</span>
-        </div>
-        {!users ? (
+
+        {!users || users.length === 0 ? (
           <div>There are no users</div>
         ) : (
-          users.map(user => (
-            <div key={user.id}>
-              <img src={user.avatarUrl} style={imgStyle} />
-              {user.email}
-              {' -  '}
-              <button
-                type="button"
-                onClick={() => this.handleUserDelete(user.id)}
-              >
-                Delete
-              </button>
-              <button
-                type="button"
-                onClick={() => this.handleToggleisAdmin(user.id)}
-              >
-                {user.isAdmin ? 'true' : 'false'}
-              </button>
-            </div>
-          ))
+          <div className="simple-flex-problems adminCenter">
+            {users.map(user => (
+              <div key={user.id}>
+                <div className="userCard">
+                  <img src={user.avatarUrl} style={imgStyle} />
+                  {user.email}
+
+                  <Button
+                    type="button"
+                    onClick={() => this.handleUserDelete(user.id)}
+                    style={buttonStyle}
+                  >
+                    Delete
+                  </Button>
+                  <Button
+                    type="button"
+                    style={buttonStyle}
+                    onClick={() => this.handleToggleisAdmin(user.id)}
+                  >
+                    {user.isAdmin ? 'Admin' : 'Not Admin'}
+                  </Button>
+                </div>
+              </div>
+            ))}
+          </div>
         )}
         <h3> Edit/Disable Problems</h3>
         {!problems || !problems.length ? (
           <div>There are no problems</div>
         ) : (
-          problems.map(problem => (
-            <div key={count++}>
-              <div>
-                {problem.id}. Descripton: {problem.description}
+          <div className="simple-flex-problems adminCenter">
+            {problems.map(problem => (
+              <div className="problemCard" key={count++}>
+                <div>
+                  {problem.id}. Descripton: {problem.description}
+                </div>
+                <div>Function Name: {problem.funcName} </div>
+                <div>
+                  Arguments:{' '}
+                  {problem.arguments.map(arg => (
+                    <span key={count++}> {arg}, </span>
+                  ))}
+                </div>
+                <div>
+                  Inputs:{' '}
+                  {problem.inputs.map(input => (
+                    <span key={count++}>
+                      [{' '}
+                      {input.map(item => <span key={count++}> {item}, </span>)}],{' '}
+                    </span>
+                  ))}
+                </div>
+                <div>
+                  Outputs:
+                  {/* {problem.outputs} */}
+                  {problem.outputs.map(output => (
+                    <span key={count++}> {output}, </span>
+                  ))}
+                </div>
+                <div>Stage: {problem.stageId}</div>
               </div>
-              <div>Function Name: {problem.funcName} </div>
-              <div>
-                Arguments:{' '}
-                {problem.arguments.map(arg => (
-                  <span key={count++}> {arg}, </span>
-                ))}
-              </div>
-              <div>
-                Inputs:{' '}
-                {problem.inputs.map(input => (
-                  <span key={count++}>
-                    [ {input.map(item => <span key={count++}> {item}, </span>)}],{' '}
-                  </span>
-                ))}
-              </div>
-              <div>
-                Outputs:
-                {/* {problem.outputs} */}
-                {problem.outputs.map(output => (
-                  <span key={count++}> {output}, </span>
-                ))}
-              </div>
-              <div>Stage: {problem.stageId}</div>
-            </div>
-          ))
+            ))}
+          </div>
         )}
       </div>
     )
