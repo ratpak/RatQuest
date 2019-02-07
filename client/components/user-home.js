a5ce671692dac8e754f42c23c247b6c0ec466457
MJ Zefeldt <mjzefeldt@gmail.com>
2019-02-07 15:45:26 -0600

    fixed load bug

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -11,31 +11,37 @@ const UserHome = props => {
   return (
     <Fragment>
       <Navbar />
-      <div id="board-wrapper">
-        <div id="rat-board-state">
-          <Link to={`/sandbox/${user.id}`}>
-            <Rat />
-          </Link>
+      {props.user && props.stage && props.solvedProblems ? (
+        <div id="board-wrapper">
+          <div id="rat-board-state">
+            <Link to={`/sandbox/${user.id}`}>
+              <Rat />
+            </Link>
+          </div>
+          <div id="board-01">
+            <Board />
+          </div>
+          <div className="stage" id="stage-box-01">
+            <HomeStage stageInHome={1} />
+          </div>
+          <div className="stage" id="stage-box-02">
+            <HomeStage stageInHome={2} />
+          </div>
+          <div className="stage" id="stage-box-03">
+            <HomeStage stageInHome={3} />
+          </div>
         </div>
-        <div id="board-01">
-          <Board />
-        </div>
-        <div className="stage" id="stage-box-01">
-          <HomeStage stageInHome={1} />
-        </div>
-        <div className="stage" id="stage-box-02">
-          <HomeStage stageInHome={2} />
-        </div>
-        <div className="stage" id="stage-box-03">
-          <HomeStage stageInHome={3} />
-        </div>
-      </div>
+      ) : (
+        <div>Loading...</div>
+      )}
     </Fragment>
   )
 }
 
 const mapState = state => ({
-  user: state.user
+  user: state.user,
+  stage: state.stage,
+  solvedProblems: state.problem.solvedProblems
 })
 
 export default connect(mapState)(UserHome)
